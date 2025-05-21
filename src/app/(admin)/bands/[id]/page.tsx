b0ae46cf7817272d334a5a6ea19cedb99ed77d47
Jorge Sant Ana <jorge@argus-academy.com>
2025-05-21 11:01:06 -0300

    :tada: feat: Server Component vs Client Component

--- src/app/(admin)/bands/[id]/page.tsx
+++ src/app/(admin)/bands/[id]/page.tsx
@@ -1,3 +1,8 @@
 export default function Page() {
+    if(typeof window !== 'undefined') {
+        console.log('Contexto do Client')
+    } else {
+        console.log('Contexto do Server')
+    }
     return <h1>Banda ID ...</h1>
 }
