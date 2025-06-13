b7aae032d40780686c25ab10adb5cfa9c9230f47
Jorge Sant Ana <jorge@argus-academy.com>
2025-06-13 12:26:59 -0300

    :tada: Feat: API (Route Handlers) - Implementando Route Handlers para os Métodos HTTP (API Rest)

---
+++ src/app/api/route.ts
@@ -0,0 +1,27 @@
+export function GET() {
+  return Response.json({ msg: "API Rest - Método GET" });
+}
+
+export function POST() {
+  return Response.json({ msg: "API Rest - Método POST" });
+}
+
+export function PUT() {
+  return Response.json({ msg: "API Rest - Método PUT" });
+}
+
+export function PATCH() {
+  return Response.json({ msg: "API Rest - Método PATCH" });
+}
+
+export function DELETE() {
+  return Response.json({ msg: "API Rest - Método DELETE" });
+}
+
+export function HEAD() {
+  return Response.json({ msg: "API Rest - Método HEAD" });
+}
+
+export function OPTIONS() {
+  return Response.json({ msg: "API Rest - Método OPTIONS" });
+}
