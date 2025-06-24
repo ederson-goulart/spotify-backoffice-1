e6fea384436d71f73aa3ab59156393f58b087fe7
Jorge Sant Ana <jorge@argus-academy.com>
2025-06-24 10:36:02 -0300

    :tada: Feat: Zod - Validação de Dados e Tratamento de Erros em Requisições HTTP Parte 2

--- src/app/api/band/route.ts
+++ src/app/api/band/route.ts
@@ -10,6 +10,8 @@ const BandSchema = z.object({
   status: z.enum(["active", "inactive"]),
 });
 
+const BandArraySchema = z.array(BandSchema).min(1);
+
 export async function GET() {
   const bands = await prisma.band.findMany();
   return Response.json(bands);
@@ -44,40 +46,38 @@ export async function GET() {
 // }
 
 // JSON (abordagem)
-// export async function POST(request: Request) {
-//   const data = await request.json();
-//   console.log(data);
-
-//   // TODO: Armazenar os dados no banco de dados
-
-//   return Response.json({ msg: "Dados recebidos com sucesso!", data });
-// }
-
-// URL Enconded (abordagem)
 export async function POST(request: Request) {
   try {
-    const bodyText = await request.text();
-    const params = new URLSearchParams(bodyText);
-    const name = params.get("name");
-    const slug = params.get("slug");
-    const description = params.get("description");
-    const status = params.get("status");
+    const data = await request.json();
 
-    // Validação dos dados
-    const validatedData = BandSchema.parse({
-      name: name,
-      slug: slug,
-      description: description || "",
-      status: status,
-    });
-
-    // TODO: Armazenar os dados no banco de dados
-
-    return Response.json({
-      msg: "URL Encoded",
-      validatedData,
-    });
+    if (Array.isArray(data)) {
+      // tratar como array
+      const validatedData = BandArraySchema.parse(data);
+      // TODO: Armazenar os dados no banco de dados
+      return Response.json({ msg: "JSON (array)", validatedData });
+    } else if (typeof data === "object" && data !== null) {
+      // tratar como item único
+      const validatedData = BandSchema.parse(data);
+      // TODO: Armazenar os dados no banco de dados
+      return Response.json({ msg: "JSON (único)", validatedData });
+    } else {
+      return Response.json(
+        { error: "Dados encaminhados em um formato inválido" },
+        { status: 400 },
+      );
+    }
   } catch (error: unknown) {
+    if (error instanceof SyntaxError) {
+      console.error(
+        "Erro de sintaxe ao ler o JSON do Body da requisição",
+        error.message,
+      );
+      return Response.json(
+        { error: "Conteúdo (body) da requisição está inválido!" },
+        { status: 400 },
+      );
+    }
+
     if (error instanceof z.ZodError) {
       return Response.json(
         { error: "Erro de validação", details: error.issues },
@@ -92,6 +92,46 @@ export async function POST(request: Request) {
     );
   }
 }
+
+// URL Enconded (abordagem)
+// export async function POST(request: Request) {
+//   try {
+//     const bodyText = await request.text();
+//     const params = new URLSearchParams(bodyText);
+//     const name = params.get("name");
+//     const slug = params.get("slug");
+//     const description = params.get("description");
+//     const status = params.get("status");
+
+//     // Validação dos dados
+//     const validatedData = BandSchema.parse({
+//       name: name,
+//       slug: slug,
+//       description: description || "",
+//       status: status,
+//     });
+
+//     // TODO: Armazenar os dados no banco de dados
+
+//     return Response.json({
+//       msg: "URL Encoded",
+//       validatedData,
+//     });
+//   } catch (error: unknown) {
+//     if (error instanceof z.ZodError) {
+//       return Response.json(
+//         { error: "Erro de validação", details: error.issues },
+//         { status: 400 },
+//       );
+//     }
+
+//     console.log("Erro desconhecido: ", error);
+//     return Response.json(
+//       { error: "Erro desconhecido (erro interno do servidor)" },
+//       { status: 500 },
+//     );
+//   }
+// }
 
 export function PUT() {
   return Response.json({ msg: "API Rest - Método PUT" });
