8fb0a492eb14b2312f2e20f30b2730aae8d79c34
Jorge Sant Ana <jorge@argus-academy.com>
2025-07-12 11:05:02 -0300

    :tada: feat: Consumindo Dados da API - Abordagem SSR

--- src/app/(admin)/bands/components/List.tsx
+++ src/app/(admin)/bands/components/List.tsx
@@ -1,17 +1,18 @@
 import Button from "@/app/components/Button";
+import { Band } from "../../../../../generated/prisma";
 
-interface Band {
-  name: string;
-  status: string;
-}
-
-const TableRow = ({ name, status }: Band) => {
+const TableRow = ({ band }: { band: Band }) => {
   return (
     <tr>
-      <td className="px-6 py-4 whitespace-nowrap text-gray-800">{name}</td>
+      <td className="px-6 py-4 whitespace-nowrap text-gray-800">{band.name}</td>
+      <td className="px-6 py-4 whitespace-nowrap text-gray-800">
+        {band.description && band.description.length > 30
+          ? `${band.description.slice(0, 30)}...`
+          : band.description}
+      </td>
       <td className="px-6 py-4 whitespace-nowrap">
         <span className="inline-flex items-center px-2 py-0.5 rounded bg-green-100 text-green-800">
-          {status}
+          {band.status}
         </span>
       </td>
       <td className="text-right font-sm space-x-4 whitespace-nowrap">
@@ -22,7 +23,9 @@ const TableRow = ({ name, status }: Band) => {
   );
 };
 
-export default function List() {
+export default async function List() {
+  const response = await fetch("http://localhost:3000/api/band");
+  const bands: Band[] = await response.json();
   return (
     <section className="overflow-x-auto p-4">
       <header className="flex justify-end mb-4">
@@ -35,14 +38,24 @@ export default function List() {
               Nome
             </th>
             <th scope="col" className="px-6 py-3">
+              Descrição
+            </th>
+            <th scope="col" className="px-6 py-3">
               Status
             </th>
             <th scope="col" className="px-6 py-3"></th>
           </tr>
         </thead>
         <tbody className="bg-white divide-y divide-gray-200">
-          <TableRow name="Henrique & Juliano" status="Ativo"></TableRow>
-          <TableRow name="Capital Inicial" status="Ativo"></TableRow>
+          {Array.isArray(bands) && bands.length > 0 ? (
+            bands.map((band) => <TableRow key={band.id} band={band} />)
+          ) : (
+            <tr>
+              <td colSpan={3} className="text-center text-gray-500 py-4">
+                Nenhum registro encontrado
+              </td>
+            </tr>
+          )}
         </tbody>
       </table>
     </section>
