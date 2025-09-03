670b67fb41b185959ae634a36c3b48670cd663c0
Jorge Sant Ana <jorge@argus-academy.com>
2025-09-03 13:13:14 -0300

    :tada: feat: Suspense Parte 1

--- src/app/(admin)/home/page.tsx
+++ src/app/(admin)/home/page.tsx
@@ -1,3 +1,13 @@
-export default function Page() {
-  return <h1>Home</h1>;
+import Indicator from "./components/Indicator";
+
+export default async function Page() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-gray-800">Home</h1>
+      <div className="flex gap-8">
+        <Indicator type="bands"></Indicator>
+        <Indicator type="tracks"></Indicator>
+      </div>
+    </>
+  );
 }
