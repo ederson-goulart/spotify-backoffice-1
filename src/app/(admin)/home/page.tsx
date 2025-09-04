b1a6900f4a2addf07261d9f7da56dfa5b025969d
Jorge Sant Ana <jorge@argus-academy.com>
2025-09-04 09:36:22 -0300

    :tada: feat: Suspense Parte 2

--- src/app/(admin)/home/page.tsx
+++ src/app/(admin)/home/page.tsx
@@ -1,12 +1,19 @@
+import { Suspense } from "react";
 import Indicator from "./components/Indicator";
+import IndicatorSkeleton from "./components/IndicatorSkeleton";
 
 export default async function Page() {
   return (
     <>
       <h1 className="text-3xl font-bold text-gray-800">Home</h1>
       <div className="flex gap-8">
-        <Indicator type="bands"></Indicator>
-        <Indicator type="tracks"></Indicator>
+        <Suspense fallback={<IndicatorSkeleton />}>
+          <Indicator type="bands" />
+        </Suspense>
+
+        <Suspense fallback={<IndicatorSkeleton />}>
+          <Indicator type="tracks" />
+        </Suspense>
       </div>
     </>
   );
